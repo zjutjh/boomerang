02f9f50f37512223d30e13adb6baf46ba7ad629b
zzhutianyu <704735206@qq.com>
2017-12-20 17:15:11 +0800

    完成vue配置

---
+++ resources/assets/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <h1>demo</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,25 +1,13 @@
+import Vue from 'vue';
+import App from './App'
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
 
-require('./bootstrap');
 
-window.Vue = require('vue');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
-/*Vue.component('example-component', require('./components/ExampleComponent.vue'));*/
-
-import Hello from './components/Hello.vue';
-
-const app = new Vue({
+new Vue({
     el: '#app',
-    render: h => h(hello)
+    template: '<App/>',
+    components: {App}
+
 });
