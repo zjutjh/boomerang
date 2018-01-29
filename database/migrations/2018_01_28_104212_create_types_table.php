5a601ffef2829df4b168df4143de7ec1c5d92701
zzhutianyu <704735206@qq.com>
2018-01-29 11:14:30 +0800

    udpate migration 暂

--- database/migrations/2018_01_28_104212_create_types_table.php
+++ database/migrations/2018_01_28_104212_create_types_table.php
@@ -1,5 +1,5 @@
-
-use Illuminate\Support\Facades\Schema;<?php
+<?php
+use Illuminate\Support\Facades\Schema;
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
