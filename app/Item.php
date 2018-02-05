26bf7051c54de11a9494427cbe5698265598d701
SilverBullete <1256819071@qq.com>
2018-02-05 14:20:35 +0800

    访问器

--- app/Item.php
+++ app/Item.php
@@ -8,13 +8,36 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use Searchable;
-    //protected $primaryKey='item_id';
 
-   /**
+    /**
+     * 获取用户的名字
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function getTypeIdAttribute($value)
+    {
+        $type = Type::where('id',$value)
+            ->first();
+        return $type;
+    }
+    public function getLostTypeAttribute($value)
+    {
+        if ($value==0)
+        {
+            return '失物招领';
+        }
+        elseif ($value==1)
+        {
+            return '寻物启事';
+        }
+    }
+    /**
      * Get the indexable data array for the model.
      *
      * @return array
      */
+
     public function toSearchableArray()
     {
         $array = $this->toArray();
@@ -23,4 +46,5 @@ class Item extends Model
 
         return $array;
     }
+
 }
