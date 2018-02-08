029f1179023d8e4e6ed9ca1d0dd9455ea732b317
SilverBullete <1256819071@qq.com>
2018-02-08 20:55:20 +0800

    路由 及和张静茜一起的SuperAdmin

--- app/Http/Controllers/SuperadminController.php
+++ app/Http/Controllers/SuperadminController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 
-class SuperadminController extends Controller
+class SuperAdminController extends Controller
 {
     public function add(Request $request)
     {
@@ -28,6 +28,29 @@ class SuperadminController extends Controller
 
     public function operatinglist()
     {
+
+    }
+
+    public function Super()
+    {
+        $user = User::whereIn('user_type',[2,3])
+            ->get();
+
+        $itemLost = Item::latest()
+            ->where('lost_type',1)
+            ->select('uid','title','description','lost_place','lost_type','type_id','images','phone','qq','status')
+            ->take(10)
+            ->get();
+        $itemFind = Item::latest()
+            ->where('lost_type',0)
+            ->select('uid','title','description','lost_place','lost_type','type_id','images','phone','qq','status')
+            ->take(10)
+            ->get();
+
+        return $this->apiReponse(200,null,
+            ['user' => $user,
+                'itemLost' => $itemLost,
+                'itemFind' => $itemFind]);
 
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,14 @@ Route::post('create-item', ['as'=>'create-item','uses'=>'ItemSearchController@cr
 Route::get('api/detail/{id}',['uses'=>'DetailController@getDetail']);
 Route::get('api/mine/detail/{uid}/{id}',['uses'=>'MydetailController@Mydetail']);
 Route::get('admin/detail/{id}',['uses'=>'AdmindetailController@getDetail']);
-Route::any('admin/super/list',['uses'=>'SuperadminController@add']);
-Route::any('admin/super/list',['uses'=>'SuperadminController@delete']);
+Route::any('admin/super/list',['uses'=>'SuperAdminController@add']);
+Route::any('admin/super/list',['uses'=>'SuperAdminController@delete']);
+Route::any('api/auto_login', 'Auth\LoginController@autoLogin');
+Route::any('api/new/lists','NewController@latest');
+Route::any('api/lost/lists','ItemController@LostList');
+Route::any('api/find/lists','ItemController@FoundList');
+Route::any('api/mine/lists','MineController@Mine');
+Route::any('admin/index','SuperAdminController@Super');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
