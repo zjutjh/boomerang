a84dc6f47940f930aed5a35049fee717ff6802b9
SilverBullete <1256819071@qq.com>
2018-02-09 14:02:43 +0800

    重名的超级管理员

--- app/Http/Controllers/SuperadminController.php
+++ app/Http/Controllers/SuperadminController.php
@@ -2,38 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\AdminLog;
 use App\Item;
 use App\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class SuperadminController extends Controller
 {
     public function add(Request $request)
     {
-        DB::table('users')
-            ->where('qq = or wechat = or phone = ',[$request,$request,$request])
+        User::where('qq = or wechat = or phone = ',[$request,$request,$request])
             ->update(['user_type' => 2]);
     }
 
     public function delete(Request $request)
     {
         $id = $request->get('id');
-        DB::table('users')
-            ->where('id',$id)
+        User::where('id',$id)
             ->update(['user_type' => 1]);
     }
 
     public function adminlist()
     {
-        DB::table('users')
-            ->where('user_type',2)
+        User::where('user_type',2)
             ->select('name');
     }
 
-    public function operatinglist()
+    public function loglist()
     {
-
+        $logs = AdminLog::latest()
+            ->get();
+        return $this->apiReponse(200,null,['logs'=>$logs]);
     }
 
 
