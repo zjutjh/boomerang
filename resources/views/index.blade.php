627cf8d88e2ae4ec98422666743b79b43d28c2ef
zzhutianyu <704735206@qq.com>
2018-03-24 20:19:32 +0800

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>精弘网络失物招领</title>
-    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" >
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="format-detection" content="telephone=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
