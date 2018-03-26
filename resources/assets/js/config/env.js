a1ff98463d5322c8e42a55bd69ebe06d62e6f322
zzhutianyu <704735206@qq.com>
2018-03-26 19:48:24 +0800

    add send_url

--- resources/assets/js/config/env.js
+++ resources/assets/js/config/env.js
@@ -1,2 +1,2 @@
-// export const api_url = "https://boomerang.zhutianyu.top"
-export const api_url = "http://localhost:3000"
+export const api_url = "https://boomerang.zhutianyu.top"
+// export const api_url = "http://localhost:3000"
