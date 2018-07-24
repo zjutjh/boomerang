7b965cdc90d27d7dfd98fecb9f6351e0efb6905a
SilverBullete <1256819071@qq.com>
2018-07-24 21:55:16 +0800

    公告表

--- database/migrations/2018_07_23_000000_create_announcement_table.php
+++ database/migrations/2018_07_23_000000_create_announcement_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatAnnouncementTable extends Migration{
+class CreateAnnouncementTable extends Migration{
     public function up(){
         Schema::create('announcement', function (Blueprint $table) {
             $table->increments('id');
